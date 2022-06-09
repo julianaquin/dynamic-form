c090e65bebc1c8ba72d32c446084fafdab6c47b1
Juliana Varela <juliana_varelaq@hotmail.com>
2022-06-08 20:19:34 -0500

    feat: add step two and three

--- src/components/DynamicForm/ThirdStep/ThirdStep.js
+++ src/components/DynamicForm/ThirdStep/ThirdStep.js
@@ -1,6 +1,25 @@
+import { Col, Container, FloatingLabel, Row, Form } from "react-bootstrap";
+import './ThirdStep.css'
 
 export default function ThirdStep() {
     return(
-        <div>ThirdStep</div>
+        <div>
+            <h2> Third Step</h2>
+            <h4> Do you want to add any comments?</h4>
+            <Container className="p-5 text-center">
+                <Row className="justify-content-md-center">
+                    <Col lg="10">
+                        <FloatingLabel controlId="floatingTextarea" label="comments" className="mb-3">
+                            <Form.Control as="textarea" placeholder="Leave a comment" onChange={(e) => console.log(e.target.value)} />
+                        </FloatingLabel>
+                    </Col>
+                </Row>
+                <div className="send-form">
+                    <p className="result p-3" onClick={() => console.log('view result')}>
+                        View Result 🤖
+                    </p>
+                </div>
+            </Container>
+        </div>
     )
 };
